a17b56e8735b6cdb1e16b35dc5e767ce2f5fb4b4
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-06-27 20:38:22 +0100

    first commit

---
+++ src/cyphers/core.clj
@@ -0,0 +1,52 @@
+(ns cyphers.core)
+
+(def start (int \a))
+(def end (int \z))
+(def start-upper (int \A))
+(def end-upper (int \Z))
+(def all-chars (concat (range start (inc end)) (range start-upper (inc end-upper))))
+
+(def alpha-length (- end start))
+
+#_(defn inc-by
+  [chr shift-by]
+  (let [idx (int chr)
+        simple-sum (+ idx shift-by)]
+    (char
+     (if (> simple-sum end)
+       (+ start
+          (mod (+ (int chr) shift-by) end))
+       simple-sum)))))
+
+(defn uppercase?
+  [chr]
+  (contains? (set (range start-upper (inc end-upper))) (int chr)))
+
+(defn inc-by
+  [shift-by chr]
+  (let [real-start (if (uppercase? chr) start-upper start)
+        idx (int chr)
+        offset (- idx real-start)
+        to-shift (mod (+ offset shift-by) (inc alpha-length))]
+
+    (char (+ real-start to-shift))))
+
+(defn gen-map
+  [shift-by]
+  (into {}
+        (for [c all-chars]
+          [(char c) (inc-by shift-by c)])))
+
+(defn subs
+  [word subs-map]
+  (clojure.string/join
+   (map #(get subs-map %) word)))
+
+(defn rot-by
+  [word shift-by]
+  (let [subs-map (gen-map shift-by)]
+    (subs word subs-map)))
+
+(def rot-by-custom subs)
+
+
---
+++ test/cyphers/core_test.clj
@@ -0,0 +1,7 @@
+(ns cyphers.core-test
+  (:require [clojure.test :refer :all]
+            [cyphers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
